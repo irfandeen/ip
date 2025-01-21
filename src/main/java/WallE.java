e614df32a95f7698d60275628070fe61770ee4d5
irfandeen <abdulwahabirfan1@gmail.com>
2025-01-21 21:06:38 +0800

    Remove unused variables in WallE.java

--- src/main/java/WallE.java
+++ src/main/java/WallE.java
@@ -1,10 +1,5 @@
 public class WallE {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String lineBreak = "_________________________________\n";
         String greeting = lineBreak
                 + "Hello! I'm Wall-E!\n"
