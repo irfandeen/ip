bf9e4bd0eaaf78d4ba0b10542965a0e6d689fb97
irfandeen <abdulwahabirfan1@gmail.com>
2025-02-02 14:33:58 +0800

    Add echo command functionality

--- src/main/java/WallE.java
+++ src/main/java/WallE.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class WallE {
     public static void main(String[] args) {
-        String lineBreak = "_________________________________\n";
+        String lineBreak = "\t_________________________________\n";
         String greeting = lineBreak
-                + "Hello! I'm Wall-E!\n"
-                + "What can I do for you?\n\n"
-                + lineBreak
-                + "Bye. Hope to see you again soon!\n"
+                + "\tHello! I'm Wall-E!\n"
+                + "\tWhat can I do for you?\n\n"
                 + lineBreak;
+        String exitMessage = lineBreak + "\tBye. Hope to see you again soon!\n" + lineBreak;
+
         System.out.print(greeting);
+        Scanner reader = new Scanner(System.in);
+        String inputCommand = reader.nextLine();
+
+        // Echoes the input, unless input == bye
+        while (!inputCommand.equals("bye")) {
+            System.out.print(lineBreak + '\t' + inputCommand + '\n' + lineBreak);
+            inputCommand = reader.nextLine();
+        }
+
+        System.out.print(exitMessage);
     }
 }
